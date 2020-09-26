712a1b30554384006bc579873d4ecc44ed288302
Buck <buck@buckrozelle.com>
2020-09-26 10:15:33 -0400

    Initial Commit

---
+++ MyLocations/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MyLocations
+//
+//  Created by Buck Rozelle on 9/26/20.
+//  Copyright © 2020 buckrozelledotcomLLC. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
